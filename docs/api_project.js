ec461ec3000fe1021c9c60b45505d2b66fba0497
Leonardo JG Silva <leosilvadev@gmail.com>
2016-02-26 23:47:30 -0300

    commit the docs

---
+++ docs/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "rh-api",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-02-27T02:36:47.162Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
